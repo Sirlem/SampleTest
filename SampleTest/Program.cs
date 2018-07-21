9c912e9b2dd46562ca88fd30f1acf6d3f46e0a3f
sirleem43 <https://github.com/>
2018-07-21 13:38:06 +0300

    Add

--- SampleTest/Program.cs
+++ SampleTest/Program.cs
@@ -20,7 +20,6 @@ namespace SampleTest
             Console.WriteLine($"{student4.firstname} {student4.lastname}              {student4.mark}   {student4.Grade}");
             Console.WriteLine($"{student5.firstname} {student5.lastname}             {student5.mark}   {student5.Grade}");
             Console.ReadLine();
-
         }
 
     }
