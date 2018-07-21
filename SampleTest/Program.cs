b236d5d674eac0f882c8be3063fa0f4bc43cc639
sirleem43 <sirleem43@outlook.com>
2018-07-21 11:13:04 +0300

    Add

--- SampleTest/Program.cs
+++ SampleTest/Program.cs
@@ -6,7 +6,20 @@ namespace SampleTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Student student1 = new Student("JOHN", "SMITH", 60);
+            Student student2 = new Student("JOE", "SMITH" ,80);
+            Student student3 = new Student("MARY", "DOE" ,77);
+            Student student4 = new Student("LIZ", "DOE", 85);
+            Student student5 = new Student("JOHN","DOE", 40);
+            Console.WriteLine("NAME                MARKS               GRADE");
+            Console.WriteLine("---------------------------------------------");
+
+            Console.WriteLine($"{student1.firstname} {student1.lastname}          {student1.Grade} {student1.mark}");
+            Console.WriteLine($"{student2.firstname} {student2.lastname}            {student2.mark}   {student2.Grade}");
+            Console.WriteLine($"{student3.firstname} {student3.lastname}             {student3.mark}   {student3.Grade}");
+            Console.WriteLine($"{student4.firstname} {student4.lastname}              {student4.mark}   {student4.Grade}");
+            Console.WriteLine($"{student5.firstname} {student5.lastname}             {student5.mark}   {student5.Grade}");
+            Console.ReadLine();
         }
     }
 }
---
+++ SampleTest/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleTest
+{
+    public class Student
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int mark;
+
+        public Student(string StudentFirstname, string StudentLastname, int StudentMarks)
+        {
+            firstname = StudentFirstname;
+            lastname = StudentLastname;
+            mark = StudentMarks;
+        }
+        public int MARKS
+        {
+            get
+            {
+                return mark;
+            }
+            set
+            {
+                mark = value;
+
+                {
+                    if (value > 0)
+                    {
+                        if (value <= 100)
+                        {
+                            mark = value;
+                        }
+                    }
+                }
+                    }
+                }
+        public string Grade
+        {
+         get
+            { 
+                string Grade = string.Empty;
+
+                if (mark >= 70)
+                {
+                    Console.WriteLine("A");
+                }
+                else if (mark >= 60)
+                {
+                    Console.WriteLine("B");
+                }
+                else if (mark >= 50)
+                {
+                    Console.WriteLine("C");
+                }
+                else
+                {
+                    Console.WriteLine("D");
+                }
+                return Grade;
+            }
+        }
+    }
+ }
+       
+        
+
+
