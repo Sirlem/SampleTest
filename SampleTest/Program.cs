84987bf61ce90143bc025fc144849d48d637087a
sirleem43 <https://github.com/>
2018-07-23 14:02:03 +0300

    Add

--- SampleTest/Program.cs
+++ SampleTest/Program.cs
@@ -11,14 +11,19 @@ namespace SampleTest
             Student student3 = new Student("MARY", "DOE" ,77);
             Student student4 = new Student("LIZ", "DOE", 85);
             Student student5 = new Student("JOHN","DOE", 40);
-            Console.WriteLine("NAME                MARKS               GRADE");
-            Console.WriteLine("---------------------------------------------");
-
-            Console.WriteLine($"{student1.firstname} {student1.lastname}          {student1.Grade} {student1.mark}");
-            Console.WriteLine($"{student2.firstname} {student2.lastname}            {student2.mark}   {student2.Grade}");
-            Console.WriteLine($"{student3.firstname} {student3.lastname}             {student3.mark}   {student3.Grade}");
-            Console.WriteLine($"{student4.firstname} {student4.lastname}              {student4.mark}   {student4.Grade}");
-            Console.WriteLine($"{student5.firstname} {student5.lastname}             {student5.mark}   {student5.Grade}");
+            
+            Console.WriteLine("NAME         MARKS       GRADE");
+            Console.WriteLine("------------------------------");
+            Console.Write($"{student1.firstname} {student1.lastname}    {student1.mark}            ");
+            Console.Write($"{student1.Grade}");
+            Console.Write($"{student2.firstname} {student1.lastname}     {student2.mark}            ");
+            Console.Write($"{student2.Grade}");
+            Console.Write($"{student3.firstname} {student3.lastname}      {student3.mark}            ");
+            Console.Write($"{student3.Grade}");
+            Console.Write($"{student4.firstname} {student4.lastname}       {student4.mark}            ");
+            Console.Write($"{student4.Grade}");
+            Console.Write($"{student5.firstname} {student5.lastname}      {student5.mark}            ");
+            Console.Write($"{student5.Grade}");
             Console.ReadLine();
         }
 
